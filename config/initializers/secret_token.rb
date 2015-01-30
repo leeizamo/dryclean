2ebdac2097dba4365c2a465a23c0fb82ab5c7bb9
leeizamo <leeizamo@gmail.com>
2015-01-30 01:10:37 -0500

    initial dryclean project commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Drycleaning::Application.config.secret_key_base = '87b424bbdc4ccf675d6d1808c2e59a59e769d1222deb46d77cc08da39974b5bb92386550ebe9c8612bf1eaff5090bd41de4c139ff4076a40896bc71a7cc28097'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Drycleaning::Application.config.session_store :cookie_store, key: '_drycleaning_session'
